b8b6dc4a08b52a0502025382bb636f2472fc2dce
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2025-04-08 03:12:39 -0300

    feat: add better settings to my editor

--- pkg/zed.nix
+++ pkg/zed.nix
@@ -1,7 +1,24 @@
 { pkgs, lib, ... }: {
   programs.zed-editor = {
     enable = true;
-    extensions = ["nix" "toml" "elixir" "make" "rust" "rs" "hs" "js" "ts"];
+    extensions = [
+      "wakatime"
+      "ocaml"
+      "scheme"
+      "rust"
+      "proto"
+      "haskell"
+      "zig"
+      "elixir"
+      "nix"
+      "make"
+      "toml"
+      "html"
+      "dockerfile"
+      "jetbrains-new-ui-icons"
+      "github-dark-default"
+    ];
+    userKeymaps = builtins.fromJSON (builtins.readFile ./zed/keymap.json);
     userSettings = {
       vim_mode = true;
       tab_size = 2;
