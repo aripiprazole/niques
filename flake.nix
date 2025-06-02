8ff9563689099c0f17864719f077320a26bda92c
Gabrielle Oliveira <you@gabx.io>
2025-06-02 11:10:59 -0300

    suicidio

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Gabi many systems configuration";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
