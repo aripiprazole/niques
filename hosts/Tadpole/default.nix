2a3e1d80bcfc44126954413685bb633180d86ace
Gabrielle Oliveira <you@gabx.io>
2025-12-23 12:20:27 -0300

    again

--- hosts/Tadpole/default.nix
+++ hosts/Tadpole/default.nix
@@ -67,13 +67,4 @@ in
       appdir = "/Applications";
     };
   };
-
-  home.sessionVariables = {
-    PATH = "$PATH:/Users/gabrielleoliveira/go/bin";
-  };
-
-  home.file.".config/1Password/ssh/agent.toml" = {
-    enable = true;
-    source = ./1password/ssh/agent.toml;
-  };
 }
--- hosts/Tadpole/gabrielleoliveira.nix
+++ hosts/Tadpole/gabrielleoliveira.nix
@@ -4,4 +4,13 @@
   home.stateVersion = "25.05";
 
   imports = [ ../../modules ];
+
+  home.sessionVariables = {
+    PATH = "$PATH:/Users/gabrielleoliveira/go/bin";
+  };
+
+  home.file.".config/1Password/ssh/agent.toml" = {
+    enable = true;
+    source = ./1password/ssh/agent.toml;
+  };
 }
