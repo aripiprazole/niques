6fc0c1caa2a8f181db43856d5e997792f52ef669
Gabrielle Oliveira <you@gabx.io>
2026-01-01 19:30:19 -0300

    add home assistant

--- hosts/Hercules/default.nix
+++ hosts/Hercules/default.nix
@@ -4,6 +4,44 @@
 
   networking.hostName = "Hercules";
 
+  services.home-assistant = {
+    enable = true;
+    extraComponents = [
+      # Components required to complete the onboarding
+      "analytics"
+      "google_translate"
+      "met"
+      "radio_browser"
+      "shopping_list"
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      "isal"
+    ];
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = {
+        bluetooth = { };
+      };
+    };
+  };
+
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.homeassistant = {
+      volumes = [ "home-assistant:/config" ];
+      environment.TZ = "Europe/Berlin";
+      # Note: The image will not be updated on rebuilds, unless the version label changes
+      image = "ghcr.io/home-assistant/home-assistant:stable";
+      extraOptions = [
+        # Use the host network namespace for all sockets
+        "--network=host"
+        # Pass devices into the container, so Home Assistant can discover and make use of them
+        "--device=/dev/ttyACM0:/dev/ttyACM0"
+      ];
+    };
+  };
+
   boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
   fileSystems."/".device = "/dev/disk/by-label/nixos";
 }
